a96fd5d383a1d0a9eea453224670318aac9a08c1
Marek Nesleha <mnesleha@gmail.com>
2025-12-23 10:15:10 +0100

    Configuration and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Hero from "./components/Hero.jsx"
+import Navbar from "./components/navbar.jsx"
+import HomeCards from "./components/HomeCards.jsx"
+import JobListings from "./components/Joblistings.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar />
+    <Hero title="Test Title" subtitle="Test Subtitle Message" />
+    <HomeCards />
+    <JobListings />
+
+
+    {/* Browse Jobs */}
+
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
     </>
+
   )
 }
 
